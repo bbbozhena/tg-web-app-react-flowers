93684726de8075c10cbfcd381e71335da630c745
bbbozhena <bbbozhenka@gmail.com>
2024-08-08 18:07:20 +0200

    fix

---
+++ src/dataStore.js
@@ -0,0 +1,16 @@
+export const products = [
+  { id: "1", title: "Rose M", price: 150, description: "Pink rose, M size" },
+  { id: "2", title: "Rose L", price: 200, description: "Red rose, L size" },
+  {
+    id: "3",
+    title: "Bouquet S",
+    price: 100,
+    description: "You can choose favourite flowers, S size",
+  },
+  {
+    id: "4",
+    title: "Spring bouquet M",
+    price: 150,
+    description: "You can choose favourite flowers, M size",
+  },
+];
