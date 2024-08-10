b740f4bb1d95ec798b6dd76cc0e4c56afeba63b1
bbbozhena <bbbozhenka@gmail.com>
2024-08-10 19:55:08 +0200

    fix

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -20,7 +20,7 @@ const ProductList = () => {
       totalPrice: getTotalPrice(addedItems),
       queryId,
     };
-    fetch("http://localhost:8000", {
+    fetch("http://localhost:8000/web-data", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
